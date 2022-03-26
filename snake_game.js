8e4fa2d99904ee502f0e529897261594c7909db3
lucasvm01 <lucasvm01@gmail.com>
2022-03-26 17:46:31 -0300

    Circle changes

--- snake_game.js
+++ snake_game.js
@@ -1,3 +1,6 @@
+let x; // mouseX coordinate
+let y; // mouseY coordinate
+
 let posX; // Central X coordinate
 let posY; // Central Y coordinate
 
@@ -18,11 +21,11 @@ function draw() {
   background(0);
   translate(canvas_size/2,canvas_size/2);
   
-  let x = (mouseX - canvas_size/2) - posX;
-  let y = (mouseY - canvas_size/2) - posY;
+  // Get mouse X and  Y coords
+  x = (mouseX - canvas_size/2) - posX;
+  y = (mouseY - canvas_size/2) - posY;
   
-  D = sqrt(sq(x) + sq(y));
-  
+  // Walking
   if(keyIsPressed){
    direction();    
   }  
@@ -31,14 +34,19 @@ function draw() {
   noFill();
   ellipse(posX,posY, 2 * r, 2 * r);
   
-  // CAN BE USED TO SUBSTITUTE SECOND X AND Y COORDS
-  // co = x * r / D; 
-  // ca = y * r / D;
-  // line(posX, posY, co + posX, ca + posY);
+  character();
+}
+
+
+function character(){
+  // Get distance from center of circle to mouse
+  D = sqrt(sq(x) + sq(y));
   
+  // Get angle of looking direction
   alfa = acos(y / D);
   beta = -acos(y / D);
-  amp = 0.1;
+  amp = 0.1; // FOV
+  
   
   if(x > 0){
     line(posX, posY, (r * sin(alfa - amp)) + posX,
@@ -51,6 +59,11 @@ function draw() {
     line(posX, posY, (r * sin(beta + amp)) + posX,
                      (r * cos(beta + amp)) + posY);
   }
+  
+  // CAN BE USED TO SUBSTITUTE SECOND X AND Y COORDS
+  // co = x * r / D; 
+  // ca = y * r / D;
+  // line(posX, posY, co + posX, ca + posY);
 }
 
 function direction(){  
