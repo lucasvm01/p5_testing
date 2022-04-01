a28d2f876465b9f833c61a673b22180a6a1a0998
lucasvm01 <lucasvm01@gmail.com>
2022-04-01 10:09:18 -0300

    apresentação fourier linear

--- fourier_series_linear.js
+++ fourier_series_linear.js
@@ -1,35 +1,66 @@
-let r = 10;
-let s = 0;
+let L; // Controla o período da função
+let a; // Receberá a quantidade n de soma da série
+let s = 0; // Receberá a soma da série
+let inp; // Controle do input do período
 
 let slider;
+let sliderScale;
 
+
+  //Aqui só é setado o básico para início do programa
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
   slider = createSlider(0,100,0);
+  sliderScale = createSlider(0,100,50);
+  L = PI; 
+  inp = createInput(PI.toString());
+  inp.input(myInputEvent);
+}
+
+// Função para input
+function myInputEvent() {
+  L = this.value();
 }
 
 function draw() {
   background(0);
   translate(width/2,height/2);
-  scale(50);
+  scale(sliderScale.value());
   
   strokeWeight(0.1);
   
+  // Desenho da função linear
   stroke(255,0,0);
-  line(-PI,PI,PI,-PI);
+  line(-L,L,L,-L);
   
+  // Controla a quantidade de somas da série
   a = slider.value();
   
   stroke(255);
   beginShape(LINES);
-  for(let x=-PI ; x<=PI ; x+=0.01){
-    for(let n=1 ; n<=a ; n++){
-      s += -1 * pow(-1,n+1) * (2/n) * sin(n * x);
+  
+  // Série de fourier no período selecionado com a soma até n definida
+  for(let x=-L ; x<=L ; x+=0.01){ // Período
+    for(let n=1 ; n<=a ; n++){ // Soma
+      s += (-1 * pow(-1,n+1) * (2/n) * sin(n * x * (PI / L)) * (1/PI * L));
     }
     
-    vertex(x,s);
+    vertex(x,s); // Desenha a função
     
     s = 0;
   }
+  
   endShape();
+  
+//   beginShape(LINES);
+//   for(let x=-L ; x<=L ; x+=0.01){
+//     for(let n=1 ; n<=a ; n++){
+//       s += (-1 * pow(-1,n+1) * (2/n) * sin(n * x ) );
+//     }
+    
+//     vertex(x,s);
+    
+//     s = 0;
+//   }
+//   endShape();
 }
