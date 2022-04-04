0052495177c40969e0a9c38f0b8bf225c36bd2ff
lucasvm01 <lucasvm01@gmail.com>
2022-04-04 08:44:51 -0300

    fourier linear grid

--- fourier_series_linear.js
+++ fourier_series_linear.js
@@ -6,12 +6,16 @@ let inp; // Controle do input do período
 let slider;
 let sliderScale;
 
+let nLine = 2;
+let axisXSize = 20;
+let axisYSize = 20;
+let graphScale = 10;
 
   //Aqui só é setado o básico para início do programa
 function setup() {
   createCanvas(600, 600);
   slider = createSlider(0,100,0);
-  sliderScale = createSlider(0,100,50);
+  sliderScale = createSlider(0,100,2);
   L = PI; 
   inp = createInput(PI.toString());
   inp.input(myInputEvent);
@@ -27,11 +31,19 @@ function draw() {
   translate(width/2,height/2);
   scale(sliderScale.value());
   
-  strokeWeight(0.1);
+  strokeWeight(1/(3 - sliderScale.value()));
+  
+  drawGrid();
   
   // Desenho da função linear
   stroke(255,0,0);
-  line(-L,L,L,-L);
+  line(-L * graphScale,L * graphScale,L * graphScale,-L * graphScale);
+  
+  createGraph();
+  
+}
+
+function createGraph(){
   
   // Controla a quantidade de somas da série
   a = slider.value();
@@ -39,28 +51,50 @@ function draw() {
   stroke(255);
   beginShape(LINES);
   
-  // Série de fourier no período selecionado com a soma até n definida
-  for(let x=-L ; x<=L ; x+=0.01){ // Período
+  for(let x = -axisXSize; x <= axisXSize; x+= 0.01){
+    
+    // Série de fourier no período selecionado com a soma até n definida
     for(let n=1 ; n<=a ; n++){ // Soma
       s += (-1 * pow(-1,n+1) * (2/n) * sin(n * x * (PI / L)) * (1/PI * L));
     }
-    
-    vertex(x,s); // Desenha a função
-    
+
+    vertex(x * graphScale,s * graphScale); // Desenha a função
+
     s = 0;
+
   }
   
   endShape();
   
-//   beginShape(LINES);
-//   for(let x=-L ; x<=L ; x+=0.01){
-//     for(let n=1 ; n<=a ; n++){
-//       s += (-1 * pow(-1,n+1) * (2/n) * sin(n * x ) );
-//     }
-    
-//     vertex(x,s);
-    
-//     s = 0;
-//   }
-//   endShape();
+
 }
+
+function drawGrid(){
+  drawXaxis();
+  drawYaxis();
+}
+
+function drawXaxis(){
+  
+  stroke(255);
+  line(0, -axisXSize * graphScale, 0, axisXSize * graphScale);
+  
+  for(let i = -axisXSize; i <= axisXSize; i ++){
+    if(i !== 0){   
+      line(i * graphScale, -nLine, i * graphScale, nLine);
+    }
+  }
+}
+
+function drawYaxis(){
+  
+  stroke(255);
+  line(-axisYSize * graphScale, 0, axisYSize * graphScale, 0);
+  
+  for(let i = -axisYSize; i <= axisYSize; i ++){
+    if(i !== 0){   
+      line(-nLine, i * graphScale, nLine, i * graphScale);
+    }
+  }
+}
+
