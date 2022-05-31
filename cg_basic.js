37203e736fbd55a2302636e6bb42f78de5959d48
lucasvm01 <lucasvm01@gmail.com>
2022-05-30 23:06:13 -0300

    Spinning cube!!

--- cg_basic.js
+++ cg_basic.js
@@ -1,6 +1,6 @@
 
 let sl;
-let time;
+let time = 0;
 
 let v = [];
 
@@ -24,8 +24,16 @@ function matrixMult(v, matrix){
   let py = v.x * matrix[1][0] + v.y * matrix[1][1] + v.z * matrix[1][2] + matrix[1][3];
   let pz = v.x * matrix[2][0] + v.y * matrix[2][1] + v.z * matrix[2][2] + matrix[2][3];
   let pw = v.x * matrix[3][0] + v.y * matrix[3][1] + v.z * matrix[3][2] + matrix[3][3];
+
+  if(pw !== 0){
+    px /= pw;
+    py /= pw;
+    pz /= pw;  
+  }
   
-  return [px, py, pz, pw];
+  let vecMult = createVector(px, py, pz);
+  
+  return vecMult;
 }
 
 function drawMesh(v1, v2, v3, v4, v5, v6, v7, v8){
@@ -50,42 +58,34 @@ function drawMesh(v1, v2, v3, v4, v5, v6, v7, v8){
 }
 
 function drawTriangle(v1, v2, v3){
-  beginShape();
-  stroke(255,0,0); 
+  stroke(255);
   line(v1.x, v1.y, v2.x, v2.y, v1.z, v2.z);
-  endShape();
-  
-  beginShape();
-  stroke(0,255,0); 
   line(v2.x, v2.y, v3.x, v3.y, v2.z, v3.z);
-  endShape();
-  
-  beginShape();
-  stroke(0,0,255); 
   line(v1.x, v1.y, v3.x, v3.y, v1.z, v3.z);
-  endShape();
 }
 
-function drawPerspective(v){
+function getProjection(v1){
   
-  for(let i=0; i<8; i++){
-    v[i].z += 3;
-    let vec = matrixMult(v[i], proj);
-    
-    if(vec !== 0){
-      vec[0] /= vec[3];
-      vec[1] /= vec[3];
-      vec[2] /= vec[3];  
-    }
-    
-    v[i] = createVector(vec[0], vec[1], vec[2]);
-  }
-  
-  return v;
+  let vecProj = matrixMult(v1, proj);
+
+  return vecProj;
 }
 
-function rotate(){
+function getRotation(v1){
+  matRotZ =[[cos(time), -sin(time) , 0, 0],
+            [sin(time) , cos(time) , 0, 0],
+            [0         , 0         , 1, 0],
+            [0         , 0         , 0, 1]];
   
+  matRotX =[[1, 0          , 0           , 0],
+            [0, cos(time/2), -sin(time/2), 0],
+            [0, sin(time/2), cos(time/2) , 0],
+            [0, 0          , 0           , 1]]; 
+  
+  vZ = matrixMult(v1, matRotZ);
+  vXZ = matrixMult(vZ, matRotX);
+  
+  return vXZ;
 }
 
 function InitializePerspective(){
@@ -96,9 +96,9 @@ function InitializePerspective(){
   fov = 1/(tan(angle)/2);
   q = zfar/(zfar-znear);
   
-  proj = [[a * fov, 0  , 0          , 0],
-          [0      , fov, 0          , 0],
-          [0      , 0  , q          , 1],
+  proj = [[a * fov, 0  , 0         , 0],
+          [0      , fov, 0         , 0],
+          [0      , 0  , q         , 1],
           [0      , 0  , -znear * q, 0]];
   
   v[0] = createVector(0 * sl, 0 * sl, 0 * sl);
@@ -111,23 +111,12 @@ function InitializePerspective(){
   v[7] = createVector(0 * sl, 0 * sl, 1 * sl);
 }
 
-function InitializeRotation(time){
-  matRotZ =[[cos(time), -sin(time) , 0, 0],
-            [sin(time) , cos(time) , 0, 0],
-            [0         , 0         , 1, 0],
-            [0         , 0         , 0, 1]];
-  
-  matRotX =[[1, 0          , 0           , 0],
-            [0, cos(time/2), -sin(time/2), 0],
-            [0, sin(time/2), cos(time/2) , 0],
-            [0, 0          , 0           , 1]]; 
-}
 
 function setup() {
   winWidth = 500;
   winHeight = 500;
-  angle = 80 * 0.5 * PI / 180;
-  zfar = 1000;
+  angle = (50 * PI / 180) * 0.5;
+  zfar = 10;
   znear = 0.1;
   
   createCanvas(winWidth, winHeight);
@@ -135,9 +124,6 @@ function setup() {
   noCursor();
   
   InitializePerspective();
-  
-  v = drawPerspective(v);
-  
 }
 
 function draw() {
@@ -148,12 +134,21 @@ function draw() {
   ellipse(mouseX - width/2, mouseY - height/2, 10);  
   scale(5);
   
-  time += 1;
+  time += 0.05;
+  
+  let vProjected = [];
+  let vTranslated = [];
+  let vRotated = [];
   
   for(let i=0 ; i<8 ; i++){
+    vRotated[i] = getRotation(v[i]);
     
+    vTranslated[i] = vRotated[i];
+    
+    vTranslated[i].z = vRotated[i].z + 10;
+    
+    vProjected[i] = getProjection(vTranslated[i]);
   }
   
-  
-  drawMesh(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]);
+  drawMesh(vProjected[0], vProjected[1], vProjected[2], vProjected[3], vProjected[4], vProjected[5], vProjected[6], vProjected[7]);
 }
